629dc9ae25a4b475a93e0d2d543f5e9f070469d5
Kristof Colpaert <kristof@kristofcolpaert.com>
2015-11-03 18:03:31 +0100

    Extra

--- Backend/Pi/PiClient.js
+++ Backend/Pi/PiClient.js
@@ -1,10 +1,41 @@
 var net = require('net'),
-    client;
-
-client = net.connect(1337, 'localhost', function() {
-   console.log('Client maakt een connectie');
+    jsonHeal = require('json-heal'),
+    client1,
+    client2;
+/**
+ * Configuration of client1.
+ */
+client1 = net.connect(1337, '192.168.0.191', function() {
+    console.log('Client1 maakt een connectie');
 });
 
-client.on('data', function(data) {
-   console.log(data.toString('utf8'));
+client1.on('data', function(data) {
+    //var distance = getDistance(JSON.parse(data.toString('utf8')));
 });
+
+/**
+ * Configuration of client2.
+ */
+client2 = net.connect(1337, '192.168.0.142', function() {
+    console.log('Client2 maakt een connectie');
+});
+
+client2.on('data', function(data) {
+    var beaconString = data.toString('utf8');
+    var beaconArray = beaconString.split(/(\}\})/);
+
+    //console.log(beaconArray);
+
+    for(i = 0, l = beaconArray.length; i < l; i++) {
+        var beaconObject = beaconArray[i];
+
+        if(beaconObject !== '}}' && beaconObject !== '') {
+            console.log(beaconObject);
+        }
+    }
+});
+
+var getDistance = function(data) {
+    var rssi = data.beacon.rssi;
+    console.log(rssi);
+};
---
+++ Backend/Pi/PiServer2.js
@@ -0,0 +1,37 @@
+/**
+ * Created by kristofcolpaert on 02/11/15.
+ */
+
+var bleacon = require('bleacon'),
+    net = require('net'),
+    sockets = [],
+    serverId = 2;
+
+/**
+ * Create a TCP server and listen to port 1337 for connections.
+ */
+var server = net.createServer(function(socket) {
+    sockets.push(socket);
+    socket.setEncoding('utf8');
+
+    socket.on('end', function(data) {
+        var socketIndex = sockets.indexOf(socket);
+        sockets.splice(socketIndex, 1);
+    });
+}).listen(1337);
+
+/**
+ * Start scanning for beacons. When a beacon is discovered, push its data to all sockets.
+ */
+bleacon.startScanning();
+
+bleacon.on('discover', function(bleacon) {
+    var bleaconObject = { 'serverId' : serverId, 'beacon' : bleacon};
+    var bleaconString = JSON.stringify(bleaconObject);
+
+    if(sockets.length != null) {
+        sockets.forEach(function(socket) {
+            socket.write(bleaconString);
+        });
+    }
+});
