fef17bb0788edfd851de244ecb82628e7ef13610
Kristof Colpaert <kristof@kristofcolpaert.com>
2015-11-05 22:47:24 +0100

    Verfijning

--- Backend/Pi/PiClient.js
+++ Backend/Pi/PiClient.js
@@ -21,7 +21,7 @@ client1.on('data', function(data) {
         var beaconObject = JSON.parse(beaconArray[i]);
         //console.log(beaconObject.beacon.major+ ": " + (Math.round(beaconObject.beacon.accuracy * 100) / 100));
 
-        if(beaconObject.beacon.major = '56796') {
+        if(beaconObject.beacon.major = '798') {
             if(test1.length < 20) {
                 test1.push(beaconObject.beacon.accuracy);
             }
@@ -29,7 +29,7 @@ client1.on('data', function(data) {
                 test1.shift();
                 test1.push(beaconObject.beacon.accuracy);
             }
-            checkDistance();
+            getPosition();
         }
     }
 });
@@ -49,7 +49,7 @@ client2.on('data', function(data) {
         var beaconObject = JSON.parse(beaconArray[i]);
         //console.log(beaconObject.beacon.major+ ": " + (Math.round(beaconObject.beacon.accuracy * 100) / 100));
 
-        if(beaconObject.beacon.major = '56796') {
+        if(beaconObject.beacon.major = '798') {
             if(test2.length < 20) {
                 test2.push(beaconObject.beacon.accuracy);
             }
@@ -57,12 +57,12 @@ client2.on('data', function(data) {
                 test2.shift();
                 test2.push(beaconObject.beacon.accuracy);
             }
-            checkDistance();
+            getPosition();
         }
     }
 });
 
-var checkDistance = function() {
+var getPosition = function() {
     var sum1 = 0;
     test1.forEach(function(element) {
         sum1 += element;
@@ -78,15 +78,15 @@ var checkDistance = function() {
     console.log(avg1);
     console.log(avg2);
 
-    avg1 = avg1 * 2;
-    avg2 = avg2 * 2;
+    var avg1 = avg1 * 1.6;
+    var avg2 = avg2 * 1.6;
 
     console.log(avg1);
     console.log(avg2);
 
-    var p1 = {x : 0, y : 0, z : 0, r : avg1};
-    var p2 = {x : 5, y : 3, z : 0, r : avg2};
-    var p3 = {x : 2.5, y : 1.5, z : 0, r : 2};
+    var p1 = {x : 1, y : 0.5, z : 0, r : avg1};
+    var p2 = {x : 5.5, y : 4, z : 0, r : avg2};
+    var p3 = {x : 3, y : 0, z : 0, r : 2};
     var test = new trilaterate(p1, p2, p3);
 
     console.log(test);
