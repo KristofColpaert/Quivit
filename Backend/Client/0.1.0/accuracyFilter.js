6feb12a2067e9aa4cc4bd054946b9e92942125dc
Kristof Colpaert <kristof@kristofcolpaert.com>
2015-11-16 21:02:30 +0100

    Save in MongoDB

---
+++ Backend/Client/0.1.0/accuracyFilter.js
@@ -0,0 +1,114 @@
+var accuracyFilter = (function() {
+    'use strict';
+
+    //Variables
+    var averageCalculator = require('./averageCalculator.js');
+    var servers;
+    var maxDifference;
+    var accuracies = [];
+
+    //Functions
+    var createAccuracyFilter = function(tempServers, tempMaxDifference) {
+        servers = tempServers;
+        maxDifference = tempMaxDifference;
+
+        servers.forEach(function(server) {
+            accuracies[server.address] = [];
+        });
+    };
+
+    var filterAccuracy = function(beaconObject, callback) {
+        if(accuracies[beaconObject.serverIp].length <= 10) {
+            averageCalculator.calculate(accuracies[beaconObject.serverIp], function(error, average) {
+                if(error) {
+                    addToAccuracy(beaconObject);
+                }
+
+                else {
+                    if(average < beaconObject.data.accuracy) {
+                        calculateBiggerPercentage(average, beaconObject, function(percentage) {
+                            if(percentage < maxDifference) {
+                                addToAccuracy(beaconObject);
+                            }
+                        });
+                    }
+
+                    else if(average >= beaconObject.data.accuracy) {
+                        calculateSmallerPercentage(average, beaconObject, function(percentage) {
+                           if(percentage < maxDifference) {
+                               addToAccuracy(beaconObject);
+                           }
+                        });
+                    }
+                }
+            });
+        }
+
+        else {
+            averageCalculator.calculate(accuracies[beaconObject.serverIp], function(error, average) {
+                if(error) {
+                    addToAccuracy(beaconObject);
+                }
+
+                else {
+                    if(average < beaconObject.data.accuracy) {
+                        calculateBiggerPercentage(average, beaconObject, function(percentage) {
+                            if(percentage < maxDifference) {
+                                deleteFirstFromAccuracy(beaconObject);
+                                addToAccuracy(beaconObject);
+                            }
+                        });
+                    }
+
+                    else if(average >= beaconObject.data.accuracy) {
+                        calculateSmallerPercentage(average, beaconObject, function(percentage) {
+                            if(percentage < maxDifference) {
+                                deleteFirstFromAccuracy(beaconObject);
+                                addToAccuracy(beaconObject);
+                            }
+                        });
+                    }
+                }
+            })
+        }
+
+        callback(makeResultArray());
+    };
+
+    var addToAccuracy = function(beaconObject) {
+        accuracies[beaconObject.serverIp].push(beaconObject.data.accuracy);
+    };
+
+    var deleteFirstFromAccuracy = function(beaconObject) {
+        accuracies[beaconObject.serverIp].shift();
+    };
+
+    var calculateBiggerPercentage = function(average, beaconObject, callback) {
+        var percentage = 100 - ((average / beaconObject.data.accuracy) * 100);
+        callback(percentage);
+    };
+
+    var calculateSmallerPercentage = function(average, beaconObject, callback) {
+        var percentage = ((average / beaconObject.data.accuracy) * 100) - 100;
+    };
+
+    var makeResultArray = function() {
+        var resultArray = [];
+        servers.forEach(function(server) {
+            var resultObject = {
+                serverIp : server.address,
+                accuracy : accuracies[server.address][accuracies[server.address].length - 1]
+            };
+            resultArray.push(resultObject);
+        });
+        return resultArray;
+    };
+
+    //Return
+    return {
+        createAccuracyFilter : createAccuracyFilter,
+        filterAccuracy : filterAccuracy
+    };
+})();
+
+module.exports = accuracyFilter;
--- Backend/Client/0.1.0/index.js
+++ Backend/Client/0.1.0/index.js
@@ -2,84 +2,58 @@
  * Created by kristofcolpaert on 09/11/15.
  */
 
-//Variables
+/*
+** Variables
+ */
 var PiClient = require('./PiClient.js');
-var averageCalculcator = require('./averageCalculator.js');
+var MongoDbClient = require('./MongoDbClient.js');
+var accuracyFilter = require('./accuracyFilter.js');
 var laterationCalculator = require('./laterationCalculator.js');
 var dataParser = require('./dataParser.js');
 
 var servers = [{ port : 1337, address : '192.168.0.193'}, { port : 1337, address : '192.168.0.101' }];
-var positions = [];
-positions['192.168.0.193'] = [];
-positions['192.168.0.101'] = [];
+var dbUrl = 'mongodb://quivitUser:Test123@quivitdb.cloudapp.net/quivitserver';
 
-//Methods
-var client = new PiClient(servers);
-client.createPiClient();
-client.on('beacon', function(data) {
+/*
+** Methods
+ */
+
+//Create an accuracyFilter.
+accuracyFilter.createAccuracyFilter(servers, 40);
+
+//Create web clients and database clients.
+var dbClient = new MongoDbClient(dbUrl);
+var webClient = new PiClient(servers);
+
+//Database client init and listeners.
+dbClient.createMongoDbClient();
+
+dbClient.on('error', function(error) {
+   console.log(error);
+});
+
+dbClient.on('result', function(result) {
+    console.log(result.result);
+});
+
+//Web client init and listeners.
+webClient.createPiClient();
+
+webClient.on('beacon', function(data) {
     var beaconData = data.split(';');
     dataParser.parse(beaconData[0], function(error, beaconObject) {
-        var currentBeaconDistance = beaconObject.data.accuracy;
-        if(positions[beaconObject.serverIp].length <= 10) {
-            averageCalculcator.calculate(positions[beaconObject.serverIp], function(error, average) {
-                if(error) {
-                    positions[beaconObject.serverIp].push(currentBeaconDistance);
-                }
+        accuracyFilter.filterAccuracy(beaconObject, function(resultArray) {
+            //Two observations, others are dummy data.
+            var positionObjects = [
+                { x : 0, y : 140, distance : ((resultArray[0].accuracy * 100) * 1.3) },
+                { x : 431, y : 264, distance : ((resultArray[0].accuracy * 100) * 1.3) },
+                { x : 150, y : 0, distance : 210 },
+                { x : 431, y : 210, distance : 281 },
+            ];
 
-                else {
-                    if(average < currentBeaconDistance) {
-                        var percentge = 100 - ((average / currentBeaconDistance) * 100);
-                        if(percentge < 30) {
-                            positions[beaconObject.serverIp].push(currentBeaconDistance);
-                        }
-                    }
-
-                    else if (average >= currentBeaconDistance) {
-                        var percentage = ((average / currentBeaconDistance) * 100) - 100;
-                        if(percentage < 30) {
-                            positions[beaconObject.serverIp].push(currentBeaconDistance);
-                        }
-                    }
-                }
+            laterationCalculator.laterate(positionObjects, function(error, position) {
+                dbClient.insertData('test', position);
             });
-        }
-
-        else {
-            averageCalculcator.calculate(positions[beaconObject.serverIp], function(error, average) {
-                if(error) {
-                    positions[beaconObject.serverIp].push(currentBeaconDistance);
-                }
-
-                else {
-                    if(average < currentBeaconDistance) {
-                        var percentge = 100 - ((average / currentBeaconDistance) * 100);
-                        if(percentge < 30) {
-                            positions[beaconObject.serverIp].shift();
-                            positions[beaconObject.serverIp].push(currentBeaconDistance);
-                        }
-                    }
-
-                    else if(average >= currentBeaconDistance) {
-                        var percentage = ((average / currentBeaconDistance) *100) - 100;
-                        if(percentage < 30) {
-                            positions[beaconObject.serverIp].shift();
-                            positions[beaconObject.serverIp].push(currentBeaconDistance);
-                        }
-                    }
-                }
-            });
-        }
-    });
-
-    //One observation, others are dummy data.
-    var positionObjects = [
-        { x : 0, y : 140, distance : ((positions['192.168.0.193'][positions['192.168.0.193'].length - 1] * 100) * 1.3) },
-        { x : 431, y : 264, distance : ((positions['192.168.0.101'][positions['192.168.0.101'].length - 1] * 100) * 1.3) },
-        { x : 150, y : 0, distance : 210 },
-        { x : 431, y : 210, distance : 281 },
-    ];
-
-    laterationCalculator.laterate(positionObjects, function(error, position) {
-        console.log(position);
+        });
     });
 });
