2a0c966c6be7f0d028168f6bd2fffc388cc0f05f
Martijn Loth <MLoth@users.noreply.github.com>
2015-11-17 20:43:59 +0100

    Meer sass
    Duidelijkere en meer Sass. Structuur in verband met het opmaken van de
    verschillende react.js components.

--- Frontend/scripts/reactTemp.js
+++ Frontend/scripts/reactTemp.js
@@ -2,19 +2,43 @@
  * Created by martijn on 16/11/15.
  */
 
-// TODO: add real react code.
-
-var Navigation = React.createClass({
+const Navigation = React.createClass({
     render: function() {
         return (
-            <nav className="navigation">
-                <h1>Quivit</h1>
-            </nav>
+            <header className="header">
+                <nav className="navigation">
+                    <h1>Quivit</h1>
+
+                    <ul className="list">
+                        <li className="menu item">live</li>
+                        <li className="menu item">about</li>
+                    </ul>
+                </nav>
+            </header>
         );
     }
 });
 
+const Main = React.createClass({
+   render: function() {
+       return (
+           <main>
+               <Navigation />
+               {/* Tijdelijk ;-) */}
+               <br/>
+               <br/>
+
+               {/* TODO: Omzetten naar custom tags */}
+               <section className="games">
+                   <h2>Live Games</h2>
+               </section>
+
+           </main>
+       );
+   }
+});
+
 ReactDOM.render(
-    <Navigation />,
-    document.getElementById('endPoint')
+    <Main />,
+    document.getElementById('root')
 );
