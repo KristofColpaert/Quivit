aaa4e952399f980a997232e0db3761a68265ab30
Martijn Loth <MLoth@users.noreply.github.com>
2015-11-18 13:23:16 +0100

    Layout overview
    Overview of the games & layout.

--- Frontend/scripts/reactTemp.js
+++ Frontend/scripts/reactTemp.js
@@ -19,12 +19,31 @@ const Navigation = React.createClass({
     }
 });
 
-const Games = React.createClass({
+const GamesPanel = React.createClass({
+    getInitialState: function() {
+        return null;
+    },
     render: function() {
         return (
             <section className="games">
                 <h2>{this.props.title}</h2>
+                {this.props.games.map(function(game) {
+                    return (game);
+                })}
             </section>
+        );
+    }
+});
+
+const Game = React.createClass({
+    render: function() {
+        return(
+            <div key={this.props.teams} className="card game">
+                <span className="score">{this.props.score}</span>
+                <h3>{this.props.teams}</h3>
+                <span className="time passed" data-timepassed={this.props.timePassed}></span>
+                {/* time passed */}
+            </div>
         );
     }
 });
@@ -34,10 +53,11 @@ const Main = React.createClass({
         return (
             <main>
                 <Navigation />
-                <section>
-
-                    <Games title={'Live Games'} />
-                    <Games title={'Finished Games'} />
+                <section className="content-holder">
+                    <GamesPanel title={'Live Games'} games={[<Game teams={'KAA Gent - RSCA'} score={'0-2'} timePassed={'67'}/>, <Game teams={'FC Barcelona - Man. City'} score={'1-3'} />, <Game teams={'Stoke City - KV Mechelen'} score={'1-0'} />, <Game teams={'Sevilla - Villareal'} score={'1-1'} />]}/>
+                    <div className="clearfix"></div>
+                    <GamesPanel title={'Finished Games'} games={[<Game teams={'België - Spanje'} score={'3-2'} />, <Game teams={'Zweden - Portugal'} score={'0-0'} />, <Game teams={'Chelsea - Swansea'} score={'0-4'} />]}/>
+                    <div className="clearfix"></div>
                 </section>
             </main>
         );
