3ce8807d665ec301a5e5b95f2eeaa13f1e38c763
Bart Callant <bart@callant.net>
2015-11-22 03:55:01 +0100

    Version 1.0

---
+++ iOSClient/README.md
@@ -0,0 +1,3 @@
+iOS Client
+=======================
+App voor iPhone die gebruikt wordt om de coordinaten door te sturen.
---
+++ iOSClient/iOSClient/iOSClient-Bridging-Header.h
@@ -0,0 +1,30 @@
+//
+//  iOSClient-Bridging-Header.h
+//  iOSClient
+//
+//  Created by Bart Callant on 20/11/15.
+//  Copyright © 2015 Bart Callant. All rights reserved.
+//
+
+#ifndef iOSClient_Bridging_Header_h
+#define iOSClient_Bridging_Header_h
+
+//#import <EstimoteSDK/EstimoteSDK.h>
+
+/*
+#import "EILIndoorLocationManager.h"
+#import "EILIndoorLocationView.h"
+#import "EILLocation.h"
+#import "EILLocationBuilder.h"
+#import "EILPoint.h"
+*/
+
+//#import "EILIndoorSDK.h"
+#import "ESTIndoorLocationManager.h"
+#import "ESTIndoorLocationView.h"
+#import "ESTLocation.h"
+#import "ESTLocationBuilder.h"
+#import "ESTPoint.h"
+#import "ESTConfig.h"
+
+#endif /* iOSClient_Bridging_Header_h */
