f3078aca4304637bc4cf6aaae0ae4615261c1fd0
Bart Callant <bart@callant.net>
2015-12-17 10:49:40 +0100

    update tests issue #12

--- Frontend/0.1.1/__tests__/components/main/GamesPanel-test.js
+++ Frontend/0.1.1/__tests__/components/main/GamesPanel-test.js
@@ -17,12 +17,30 @@ describe('GamesPanel', () =>
 
     beforeEach(() =>
     {
-        var comp = TestUtils.renderIntoDocument(<GamesPanel title="Test Title" games={[]} />);
-        gamesPanelNode = ReactDOM.findDOMNode(comp);
+        gamesPanelNode =  TestUtils.renderIntoDocument(<GamesPanel title="Test Title" games={[]} />);
+        //gamesPanelNode = ReactDOM.findDOMNode(comp);
     });
 
     it('To Be Defined', () =>
     {
         expect(gamesPanelNode).toBeDefined();
+    });
+
+    it('Should have an H3 element', () =>
+    {
+        var h2 = TestUtils.scryRenderedDOMComponentsWithTag(gamesPanelNode, 'h2');
+        expect(h2.length).toBeGreaterThan(0)
+    });
+
+    it('Text in H3 should be "Test Title"', () =>
+    {
+        var firstH2 = TestUtils.scryRenderedDOMComponentsWithTag(gamesPanelNode, 'h2')[0];
+        expect(firstH2.innerHTML).toBe("Test Title");
+    });
+
+    it('Defined games to be 0, so there should be 1 child element', () =>
+    {
+        var gpNode = ReactDOM.findDOMNode(gamesPanelNode);
+        expect(gpNode.children.length).toBe(1);
     });
 });
