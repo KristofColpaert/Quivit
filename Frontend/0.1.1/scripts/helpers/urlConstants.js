37b531fd9813dc842f4ddd7028649ef1afdab3ad
Kristof Colpaert <kristof@kristofcolpaert.com>
2016-01-14 16:27:52 +0100

    Test

--- Frontend/0.1.1/scripts/helpers/urlConstants.js
+++ Frontend/0.1.1/scripts/helpers/urlConstants.js
@@ -5,8 +5,9 @@ var constants = {
     baseApiGameUrl : 'http://localhost:3000/api/game/',
     baseApiGameTodayUrl : 'http://localhost:3000/api/game/' + new Date().getFullYear() + '/' + (new Date().getMonth() + 1)
         + '/' + ((new Date().getDate() < 10) ? ('0' + new Date().getDate()) : (new Date().getDate()))  + '/excluded',
-    baseApiGamePastUrl : 'http://localhost:3000/api/game/' + new Date().getFullYear() + '/' + (new Date().getMonth() + 1)
-    + '/' + ((new Date().getDate() < 10) ? ('0' + new Date().getDate()) : (new Date().getDate()))  + '/past',
+    baseApiGamePastUrl : 'http://localhost:3000/api/game/' + new Date().getFullYear()
+        + '/' + ((new Date().getMonth() < 9) ? ('0' + (new Date().getMonth() + 1)) : (new Date().getMonth()))
+        + '/' + ((new Date().getDate() < 10) ? ('0' + new Date().getDate()) : (new Date().getDate()))  + '/past',
     baseApiEstimoteLocationUrl : 'http://localhost:3000/api/estimoteLocation/',
     baseApiUserUrl : 'http://localhost:3000/api/user',
     socketsUrl : 'http://localhost:3000',
